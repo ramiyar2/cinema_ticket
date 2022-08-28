3d4a0a9b69e8a14ba490d1657a2fe8637eefef9f
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-08-29 01:43:53 +0300

    Login Screen

---
+++ lib/auth/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
