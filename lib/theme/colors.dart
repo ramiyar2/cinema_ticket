370e7f99ddf32e53555a0ac6e39e2095eb40c0c6
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-08-29 02:13:27 +0300

    Colors

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class MyColors {
-  Color darkerBlue = const Color.fromRGBO(3, 23, 36, 1);
-  Color darkBlue = const Color.fromRGBO(6, 40, 61, 1);
-  Color blue = const Color.fromRGBO(19, 99, 223, 1);
-  Color white = Colors.white;
-  Color whiteOp = const Color.fromRGBO(255, 255, 255, 0.5);
-}
+Color darkerBlue = const Color.fromRGBO(3, 23, 36, 1);
+Color darkBlue = const Color.fromRGBO(6, 40, 61, 1);
+Color blue = const Color.fromRGBO(19, 99, 223, 1);
+Color white = Colors.white;
+Color whiteOp = const Color.fromRGBO(255, 255, 255, 0.5);
+Color bgWhite = const Color.fromRGBO(248, 248, 248, 1);
+Color bg = Get.isDarkMode ? darkerBlue : bgWhite;
