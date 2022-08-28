f6c831e784096b5c2d370bdc0e5828a569e8003d
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-08-29 02:32:23 +0300

    icons

--- lib/theme/icons.dart
+++ lib/theme/icons.dart
@@ -1,2 +1,11 @@
 String logoLight = 'assets/img/logo_light.png';
 String logoDark = 'assets/img/logo_light.png';
+// icons
+String homeIcon = 'assets/icon/home.png';
+String homeIconOutline = 'assets/icon/home_outline.png';
+String userIcon = 'assets/icon/user.png';
+String userIconOutline = 'assets/icon/user_outline.png';
+String bookmarkIcon = 'assets/icon/bookmark.png';
+String bookmarkIconOutline = 'assets/icon/bookmark_outline.png';
+String ticketIcon = 'assets/icon/ticket.png';
+String ticketIconOutline = 'assets/icon/ticket_outline.png';
