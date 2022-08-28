dec6390d9cd8cbf87cd4be9e0e2e0c602a96c46a
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-08-28 22:42:20 +0300

    basic files

---
+++ lib/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  Color darkerBlue = const Color.fromRGBO(3, 23, 36, 1);
+  Color darkBlue = const Color.fromRGBO(6, 40, 61, 1);
+  Color blue = const Color.fromRGBO(19, 99, 223, 1);
+  Color white = Colors.white;
+  Color whiteOp = const Color.fromRGBO(255, 255, 255, 0.5);
+}
