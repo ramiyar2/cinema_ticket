0617ad9bf6537a7316984f8b9e30b5097735ca6d
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-08-29 06:06:59 +0300

    Social media widget

---
+++ lib/widget/social_media.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../theme/colors.dart';
+import '../theme/icons.dart';
+
+class SocialMedia extends StatelessWidget {
+  const SocialMedia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () {},
+          child: CircleAvatar(
+            backgroundColor: darkBlue,
+            child: Image.asset(
+              facebook,
+              width: 25,
+              height: 25,
+              color: white,
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: CircleAvatar(
+            backgroundColor: darkBlue,
+            child: Image.asset(
+              twitter,
+              width: 25,
+              height: 25,
+              color: white,
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        GestureDetector(
+          onTap: () {},
+          child: CircleAvatar(
+            backgroundColor: darkBlue,
+            child: Image.asset(
+              google,
+              width: 25,
+              height: 25,
+              color: white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
