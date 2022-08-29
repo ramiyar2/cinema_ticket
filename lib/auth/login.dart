2619fd2e9442bd48b3b2f833386b957b1fb83fd5
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-08-29 04:23:04 +0300

    Login Screen

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -1,6 +1,9 @@
+import 'package:cinema_ticket/theme/colors.dart';
 import 'package:cinema_ticket/theme/icons.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../theme/theme.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
@@ -12,12 +15,98 @@ class Login extends StatelessWidget {
       body: SafeArea(
         child: Container(
           color: Theme.of(context).backgroundColor,
+          padding:
+              const EdgeInsets.only(top: 40, right: 20, left: 20, bottom: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.asset(Get.isDarkMode ? logoLight : logoDark),
-                  const Text('Cinema Ticket')
+                  Image.asset(
+                    Get.isDarkMode ? logoLight : logoDark,
+                    width: 100,
+                    height: 100,
+                  ),
+                  Text(
+                    'Cinema Ticket',
+                    style: Themes().headingStyle,
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Welcome ! \n Back',
+                style: Themes().subHeadingStyle,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Log in',
+                style: Themes().importantInfo,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: CircleAvatar(
+                      backgroundColor: darkBlue,
+                      child: Image.asset(
+                        userIcon,
+                        width: 30,
+                        height: 30,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: CircleAvatar(
+                      backgroundColor: darkBlue,
+                      child: Image.asset(
+                        userIcon,
+                        width: 30,
+                        height: 30,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: CircleAvatar(
+                      backgroundColor: darkBlue,
+                      child: Image.asset(
+                        userIcon,
+                        width: 30,
+                        height: 30,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Divider(
+                    color: bgWhite,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                    child: Text('OR'),
+                  ),
+                  Divider(
+                    color: bgWhite,
+                  ),
                 ],
               )
             ],
