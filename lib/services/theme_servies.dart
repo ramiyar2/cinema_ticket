c08fc5e9bfbdbb82761c10851860804b6aef0c9c
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-08-29 03:53:24 +0300

    Theme Servies

--- lib/services/theme_servies.dart
+++ lib/services/theme_servies.dart
@@ -1,1 +1,18 @@
-class ThemeServies {}
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class ThemeServies {
+  final GetStorage _box = GetStorage();
+  final _key = 'isDarkMode';
+  _saveThemeFromBox(bool isDarkMode) => _box.write(_key, isDarkMode);
+
+  bool _loadThemeFromBox() => _box.read<bool>(_key) ?? false;
+
+  ThemeMode get theme => _loadThemeFromBox() ? ThemeMode.dark : ThemeMode.light;
+
+  void switchTheme() {
+    Get.changeThemeMode(_loadThemeFromBox() ? ThemeMode.light : ThemeMode.dark);
+    _saveThemeFromBox(!_loadThemeFromBox());
+  }
+}
