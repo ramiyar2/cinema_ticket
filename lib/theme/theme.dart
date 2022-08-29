f79b7a70e648378ac0b69b8fc06bb9afd18fd8dd
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-08-29 03:35:37 +0300

    theme

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -7,14 +7,63 @@ class Themes {
     backgroundColor: bgWhite,
     brightness: Brightness.light,
   );
+
   static ThemeData darkTheme = ThemeData(
     backgroundColor: darkerBlue,
     brightness: Brightness.dark,
   );
-  TextStyle get headline {
+
+  TextStyle get headingStyle {
     return TextStyle(
       color: Get.isDarkMode ? white : darkBlue,
-      fontSize: 24, //36,48bold
+      fontSize: 24,
+      fontFamily: 'Cairo',
+      fontWeight: FontWeight.normal,
+    );
+  }
+
+  TextStyle get subHeadingStyle {
+    return TextStyle(
+      color: Get.isDarkMode ? white : darkBlue,
+      fontSize: 36,
+      fontFamily: 'Cairo',
+      fontWeight: FontWeight.normal,
+    );
+  }
+
+  TextStyle get importantInfo {
+    return TextStyle(
+      color: Get.isDarkMode ? white : darkBlue,
+      fontSize: 48,
+      fontFamily: 'Cairo',
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  TextStyle get titleStyle {
+    return TextStyle(
+      color: Get.isDarkMode ? white : darkBlue,
+      fontSize: 32,
+      fontFamily: 'Cairo',
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  TextStyle get subTitleStyle {
+    return TextStyle(
+      color: Get.isDarkMode ? white : darkBlue,
+      fontSize: 48,
+      fontFamily: 'Cairo',
+      fontWeight: FontWeight.normal,
+    );
+  }
+
+  TextStyle get paraStyle {
+    return TextStyle(
+      color: Get.isDarkMode ? white : darkBlue,
+      fontSize: 14,
+      fontFamily: 'Cairo',
+      fontWeight: FontWeight.normal,
     );
   }
 }
