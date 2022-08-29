c446d14a5e00ecf3260a2c8081affabcadb5a654
ramiyar2 <56196099+ramiyar2@users.noreply.github.com>
2022-08-29 04:51:28 +0300

    Login Screen

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -18,123 +18,133 @@ class Login extends StatelessWidget {
           padding:
               const EdgeInsets.only(top: 40, right: 20, left: 20, bottom: 20),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.asset(
-                    Get.isDarkMode ? logoLight : logoDark,
-                    width: 100,
-                    height: 100,
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        Get.isDarkMode ? logoLight : logoDark,
+                        width: 100,
+                        height: 100,
+                      ),
+                      Text(
+                        'Cinema Ticket',
+                        style: Themes().headingStyle,
+                      )
+                    ],
                   ),
                   Text(
-                    'Cinema Ticket',
-                    style: Themes().headingStyle,
-                  )
-                ],
-              ),
-              Text(
-                'Welcome ! \n Back',
-                style: Themes().subHeadingStyle,
-              ),
-              Text(
-                'Log in',
-                style: Themes().importantInfo,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {},
-                    child: CircleAvatar(
-                      backgroundColor: darkBlue,
-                      child: Image.asset(
-                        userIcon,
-                        width: 30,
-                        height: 30,
+                    'Welcome ! \n Back',
+                    style: Themes().subHeadingStyle,
+                  ),
+                  Text(
+                    'Log in',
+                    style: Themes().importantInfo,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: CircleAvatar(
+                          backgroundColor: darkBlue,
+                          child: Image.asset(
+                            userIcon,
+                            width: 30,
+                            height: 30,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  GestureDetector(
-                    onTap: () {},
-                    child: CircleAvatar(
-                      backgroundColor: darkBlue,
-                      child: Image.asset(
-                        userIcon,
-                        width: 30,
-                        height: 30,
+                      const SizedBox(
+                        width: 20,
                       ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  GestureDetector(
-                    onTap: () {},
-                    child: CircleAvatar(
-                      backgroundColor: darkBlue,
-                      child: Image.asset(
-                        userIcon,
-                        width: 30,
-                        height: 30,
+                      GestureDetector(
+                        onTap: () {},
+                        child: CircleAvatar(
+                          backgroundColor: darkBlue,
+                          child: Image.asset(
+                            userIcon,
+                            width: 30,
+                            height: 30,
+                          ),
+                        ),
                       ),
-                    ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: CircleAvatar(
+                          backgroundColor: darkBlue,
+                          child: Image.asset(
+                            userIcon,
+                            width: 30,
+                            height: 30,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Divider(
-                      color: whiteOp,
-                      thickness: 1,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 20, horizontal: 20),
-                    child: Text(
-                      'OR',
-                      style: Themes().subTitleStyleOP,
-                    ),
-                  ),
-                  Expanded(
-                    child: Divider(
-                      color: whiteOp,
-                      thickness: 1,
-                    ),
-                  ),
-                ],
-              ),
-              const TextField(),
-              const TextField(),
-              Text(
-                'Forget passward ?',
-                style: Themes().subTitleStyleOP,
-              ),
-              ElevatedButton(
-                  onPressed: () {},
-                  child: Row(
+                  Row(
                     children: [
                       Expanded(
-                        child: Text(
-                          'Log in',
-                          style: Themes().headingStyle,
+                        child: Divider(
+                          color: whiteOp,
+                          thickness: 1,
                         ),
                       ),
                       Padding(
-                          padding: const EdgeInsets.all(15),
-                          child: Image.asset(
-                            userIconOutline,
-                            width: 25,
-                            height: 30,
-                          ))
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 20),
+                        child: Text(
+                          'OR',
+                          style: Themes().subTitleStyleOP,
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          color: whiteOp,
+                          thickness: 1,
+                        ),
+                      ),
                     ],
-                  ))
+                  ),
+                  const TextField(),
+                  const TextField(),
+                  Text(
+                    'Forget passward ?',
+                    style: Themes().subTitleStyleOPSmall,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {},
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              'Log in',
+                              style: Themes().headingStyle,
+                            ),
+                          ),
+                          Padding(
+                              padding: const EdgeInsets.all(15),
+                              child: Image.asset(
+                                userIconOutline,
+                                width: 25,
+                                height: 30,
+                              ))
+                        ],
+                      ))
+                ],
+              ),
+              Row(
+                children: [
+                  Text('I don\'t have account,'),
+                  GestureDetector(child: Text(' Create new')),
+                ],
+              ),
             ],
           ),
         ),
